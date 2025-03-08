21226ce824e438f39160f7d7a1f4f95b56453d52
Aman Gupta <kumarguptaaman896@gmail.com>
2025-03-08 16:29:40 +0530

    Update files and add new features

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/aman99dex/slack-file-bot
 
 go 1.22.6
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/slack-go/slack v0.16.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,50 @@
 package main
 
-import(
-	"fmt"
-	"os"
-	"github.com/slack.go/slack"
+import (
+    "fmt"
+    "log"
+    "os"
+    "github.com/slack-go/slack"
 )
 
-func main(){
+func main() {
+    // Get environment variables
+    botToken := os.Getenv("SLACK_BOT_TOKEN")
+    channelID := os.Getenv("CHANNEL_ID")
 
+    // Validate environment variables
+    if botToken == "" {
+        log.Fatal("SLACK_BOT_TOKEN is not set")
+    }
+    if channelID == "" {
+        log.Fatal("CHANNEL_ID is not set")
+    }
+
+    // Initialize Slack API client
+    api := slack.New(botToken)
+
+    // File to upload
+    fileArr := []string{"Attention Is All You Need.pdf"}
+    channelArr := []string{channelID}
+
+    for i := 0; i < len(fileArr); i++ {
+        // Verify file exists
+        if _, err := os.Stat(fileArr[i]); os.IsNotExist(err) {
+            log.Fatalf("File %s does not exist", fileArr[i])
+        }
+
+        // Upload file
+        params := slack.FileUploadParameters{
+            Channels: channelArr,
+            File:     fileArr[i],
+        }
+
+        file, err := api.UploadFile(params)
+        if err != nil {
+            log.Fatalf("Failed to upload file: %v", err)
+        }
+
+        fmt.Printf("Successfully uploaded file: %s\nURL: %s\n", file.Name, file.URL)
+    }
 }
+
